0957feab03ba4c838465360d97112aeb0c4bc5d0
TheRioDeal <river.o.engum@gmail.com>
2022-01-23 12:06:51 -0800

    Update LinearActuatorControl.ino
    Completed Linear Actuator Control Test with motor controller and arduino joystick. With ~4V supplied to motor, traveled 1.9 inches in 16 seconds. Approximately 1/8" per Second

--- LinearActuatorControl/LinearActuatorControl.ino
+++ LinearActuatorControl/LinearActuatorControl.ino
@@ -1,19 +1,74 @@
-const int For = 9;
-const int Rev = 8;
-const int PWM = 13;
+const int XJoy = A1;
+const int YJoy = A0;
+const int LinAct = 6;
+const int LinFor = 7;
+const int LinRev = 8;
+double xpos;
+double ypos;
+const double DeadZP = .05;
+double LinActPow;
+int Dir = 0;  // 0 = Neutral, 1 = Forward, 2 = Reverse
+
 
 void setup() {
- Serial.begin(57600);
+ Serial.begin(9600);
   // put your setup code here, to run once:
-  pinMode(For, INPUT);
-  pinMode(Rev, INPUT);
-  pinMode(PWM, OUTPUT);
+  pinMode(LinAct, OUTPUT);
+  pinMode(LinFor, INPUT);
+  pinMode(LinRev, INPUT);
+  pinMode(XJoy, INPUT);     // X position of Joystick, 0 to 1023                 
+  pinMode(YJoy, INPUT);     // Y position of Joystick, 0 to 1023 
 
-  digitalWrite(For, HIGH);
-  digitalWrite(Rev, LOW);
+  digitalWrite(LinFor, LOW);
+  digitalWrite(LinRev, LOW);
   
 }
 
 void loop() {
-  analogWrite(PWM,255);
+  xpos = analogRead(XJoy);
+  ypos = analogRead(YJoy);
+
+  Serial.print("Joystick Position (x,y): ");
+  Serial.print(xpos,0);
+  Serial.print(", ");
+  Serial.println(ypos,0);
+
+  if(xpos > 511.5)
+  {
+    LinActPow = map(xpos,512,1023,178,255);
+    digitalWrite(LinFor, HIGH);
+    digitalWrite(LinRev, LOW);
+    Dir = 1;
+  }
+  else
+  {
+    xpos = 1023 - xpos;
+    LinActPow = map(xpos,512,1023,178,255);
+    digitalWrite(LinFor, LOW);
+    digitalWrite(LinRev, HIGH);
+    Dir = 2;
+  }
+  
+  if(xpos > (511.5-511.5*DeadZP) && xpos < (511.5+511.5*DeadZP))
+  {
+    LinActPow = 0;  
+    Dir = 0;
+  }  
+  analogWrite(LinAct,LinActPow);
+  
+  if(Dir == 0)
+    Serial.println("NEUTRAL");
+  else if(Dir == 1)
+    Serial.println("FORWARD");
+  else
+    Serial.println("REVERSE");
+    
+  Serial.print("Linear Actuator Power: ");
+  Serial.print((LinActPow/255)*100);
+  Serial.println("%");
+  
+  Serial.println("");
+  Serial.println("");
+
+  delay(200);
 }
