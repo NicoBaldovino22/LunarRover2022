8d57c821c8d794f6188d7369f79cc33f056523e9
NicoBaldovino22 <nico.baldovino@oit.edu>
2022-01-27 13:28:45 -0800

    (1/27) Update
    Finished the servo programming for turning and stopping continuous rotation.

--- joystick_test_1motor/joystick_test_1motor.ino
+++ joystick_test_1motor/joystick_test_1motor.ino
@@ -1,75 +1,69 @@
 #include <Servo.h>
 Servo servo1;
 Servo servo2;
-int x_key = A1;                                               
-int y_key = A0;                                               
-int x_pos;
-int y_pos; 
-int servo1_pin = 3;
-int servo2_pin = 5;   
-int initial_position = 90;
-int initial_position1 = 90;
+const int xJoy = A1;                                               
+const int yJoy = A0;                                               
+double x_pos;
+double y_pos; 
+const int servo1_pin = 3;
+const int servo2_pin = 5;   
+//double initial_positionx = 90;
+//double initial_positiony = 90;
+int x = 90;
+int y = 90;
 
 void setup ( ) {
 Serial.begin (9600) ;
 servo1.attach (servo1_pin);
 servo2.attach (servo2_pin);
-servo1.write (initial_position);
-servo2.write (initial_position1);
-pinMode (x_key, INPUT) ;                     
-pinMode (y_key, INPUT) ;                      
+//servo1.write (initial_positionx);
+//servo2.write (initial_positiony);
+pinMode (xJoy, INPUT) ;                     
+pinMode (yJoy, INPUT) ;                      
 }
 
 void loop ( ) {
-  x_pos = analogRead (x_key) ;    // X position of Joystick, 0 to 1023      
-  y_pos = analogRead (y_key) ;    // Y position of Joystick, 0 to 1023                  
+  x_pos = analogRead (xJoy) ;    // X position of Joystick, 0 to 1023, with 508 being at rest. Controlling Tiankongrc Digital Servo (360 degree)
+  y_pos = analogRead (yJoy) ;    // Y position of Joystick, 0 to 1023, with 508 being at rest. Controlling MicroServo 9g (180 degree)                  
 
-  if (x_pos < 300)
+  Serial.print("Joystick Position (x,y): ");
+  Serial.print(x_pos);
+  Serial.print(", ");
+  Serial.println(y_pos);
+
+  if (x_pos < 400)
   {
-    if (initial_position < 10) 
-    { } 
-    else
-    {
-      initial_position = initial_position - 20; 
-      servo1.write ( initial_position ) ; 
-      delay (100) ; 
-    } 
+    x = 180;
+    servo1.write(x);
   }
-  
-  if (x_pos > 700)
+  else if (x_pos > 600)
   {
-    if (initial_position > 360)
-    { }  
-    else
-    {
-    initial_position = initial_position + 20;
-    servo1.write ( initial_position ) ;
-    delay (100) ;
-    }
+    x = 0;
+    servo1.write(x);
   }
-  
-  if (y_pos < 300)
+  else
   {
-    if (initial_position1 < 10)
-    { }
-      else
-      {
-        initial_position1 = initial_position1 - 20; 
-        servo2.write ( initial_position1 ) ; 
-        delay (100) ;
-      }
+    x = 90;
+    servo1.write(x);
   }
+
   
-  if (y_pos > 700)
+  if (y_pos > 550)
   {
-    if (initial_position1 > 360)
-    { }        
-    else
-    {
-      initial_position1 = initial_position1 + 20;
-      servo2.write ( initial_position1 ) ;
-      delay (100) ;
-    }
+    y = 0;
   }
-  
+  else if (y_pos < 500)
+  {
+    y = 180;
+  }
+  else
+  {
+    y = 90;
+  }
+  servo2.write(y); 
+     
+  Serial.print("Output (x,y): ");
+  Serial.print(x);
+  Serial.print(", ");
+  Serial.println(y);
 }
